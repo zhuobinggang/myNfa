45d57c87165892afd7eaff3242aa78dc3f5ba384
音無マリア <1031908624@qq.com>
2016-12-31 23:14:14 +0800

    Create Token.java

---
+++ src/nfa/Token.java
@@ -0,0 +1,11 @@
+package nfa;
+
+/**
+ * Created by kobako on 2016/12/31.
+ * Just a game
+ */
+public class Token {
+    public WordType type;
+    public String value;
+    public boolean available = true; //这个token是否合法
+}
