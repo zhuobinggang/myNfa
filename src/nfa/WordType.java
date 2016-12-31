02d1c4574dc8ffd8ce5d2fd5a63792baa5611258
音無マリア <1031908624@qq.com>
2016-12-31 23:13:25 +0800

    Create WordType.java

---
+++ src/nfa/WordType.java
@@ -0,0 +1,10 @@
+package nfa;
+
+/**
+ * Created by kobako on 2016/12/31.
+ * Just a game
+ */
+public enum WordType {
+    //基本字   标识符     常量     运算符    界符
+    BASICWORD,IDENTIFIER,CONSTANT,OPERATOR,DELIMITER
+}
